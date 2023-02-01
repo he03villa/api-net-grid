c0d49d418c1863df14e42f090c4874084bcabc9f
he03villa <he03villa@gmail.com>
2023-02-01 17:19:40 -0500

    arreglando

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
